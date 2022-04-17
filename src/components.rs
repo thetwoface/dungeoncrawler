00fa0d755172fd89c5c4f1bec2116f4859c3a641
mike.scherbak <mike.scherbak@playngo.com>
2022-04-17 14:02:57 +0200

    player's field of view

--- src/components.rs
+++ src/components.rs
@@ -1,5 +1,7 @@
 pub use crate::prelude::*;
 
+use std::collections::HashSet;
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Render {
     pub color: ColorPair,
@@ -44,3 +46,28 @@ pub struct Item;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct AmuletOfYala;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct FieldOfView {
+    pub visible_tiles: HashSet<Point>,
+    pub radius: i32,
+    pub is_durty: bool,
+}
+
+impl FieldOfView {
+    pub fn new(radius: i32) -> Self {
+        Self {
+            visible_tiles: HashSet::new(),
+            radius,
+            is_durty: true,
+        }
+    }
+
+    pub fn clone_dirty(&self) -> Self {
+        Self {
+            visible_tiles: HashSet::new(),
+            radius: self.radius,
+            is_durty: true,
+        }
+    }
+}
